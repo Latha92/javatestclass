105431d2e9f137e2398f0c3145404442547bb111
AshaLatha <sfdclatha@gmail.com>
2018-12-31 17:53:35 +0530

    codechanges2 by latha

---
+++ testclass/src/testclass/testclass.java
@@ -0,0 +1,13 @@
+package testclass;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		try {
+			
+			System.out.println("Now the output is redirected!");
+		} catch(Exception e) {}
+
+	}
+
+}
